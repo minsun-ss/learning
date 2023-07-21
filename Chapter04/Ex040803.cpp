5fdd93fe5474a865c287ab3bcd7361bd7beeef8d
minsun-ss <minsun@stuffofminsun.com>
2023-07-20 22:42:08 -0400

    more for loops

---
+++ Chapter04/Ex040803.cpp
@@ -0,0 +1,9 @@
+// We would like to compute the integral 
+// exp(-t^2///t2)dt.
+// By making the susbtitution t = x+1-1/s, we can see that this is equal
+// to the integral from 0 to 1 1/s^2 exp(-(1+1-1/s)^2/2 ds
+// Write a function that uses a for loop to compute this second integral
+// using the rectangle rule.
+
+
+
