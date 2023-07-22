892a3795be521fc248f181869df6271660953255
minsun-ss <minsun@stuffofminsun.com>
2023-07-22 13:06:11 -0400

    man math

--- Chapter04/Ex040803.cpp
+++ Chapter04/Ex040803.cpp
@@ -5,5 +5,52 @@
 // Write a function that uses a for loop to compute this second integral
 // using the rectangle rule.
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double rectanglerule(double);
+double infiniteIntegral(double);
+
+int main() {
+    cout << rectanglerule(5) << endl; 
+    cout << infiniteIntegral(5) << endl;
+    return 0;
+}
+
+double rectanglerule(double x) {
+    double a = 0;
+    double b = 1;
+    int n = 1000;
+    double rectresult = 0;
+    double h = (b-a)/n;
+    for (int i=0; i<n; i++) {
+        double s = (h*(i+0.5)+a);
+        double t = x + 1 - 1/s;
+        double f = pow(s, -2) * exp(-0.5*t*t);
+        rectresult += f;
+    }
+    return rectresult/n;
+}
+
+
+double infiniteIntegral( double x ) {
+    // we perform the substitution
+    // x + 1 - 1/s;
+    // to change the infinite integral to an integral between 0 and 1
+    double a = 0;
+    double b = 1;
+    int N = 1000;
+    double h = (b-a)/N;
+    double total = 0.0;
+    for (int i=0; i<N; i++) {
+        double s = (i+0.5)*h + a;
+        double t = x + 1 - 1/s;
+        double f = pow(s,-2) * exp( - 0.5*t*t );
+        total +=f;
+    }
+    return total/N;
+}
+
 
 
