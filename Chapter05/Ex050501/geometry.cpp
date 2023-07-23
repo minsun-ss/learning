80aadf25aeb1e4d688147fa0663d0dfaa7dc8d69
minsun-ss <minsun@stuffofminsun.com>
2023-07-23 14:21:33 -0400

    Exercise

---
+++ Chapter05/Ex050501/geometry.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+using namespace std;
+
+double radius() {
+    cout << "radius run" << endl;
+    return 1.0;
+}
+
---
+++ Chapter05/Ex050501/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "geometry.h"
+using namespace std;
+
+
+int main() {
+    cout << radius() << endl;
+    return 0;
+}
