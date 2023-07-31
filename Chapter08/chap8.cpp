d2d7b1a44bab0e09c48ec82ad6f5491da9a01dfd
minsun-ss <minsun@stuffofminsun.com>
2023-07-31 07:36:02 -0400

    Working on problem 1

--- Chapter08/chap8.cpp
+++ Chapter08/chap8.cpp
@@ -1,7 +1,20 @@
 #include "chap8.h"
 
 /* 
- * Ex. 8.8.2
+ * Ex. 8.6.1
  */
-class Polynomial {
-}
+class BlackScholesModel {
+public:
+    double stockPrice;
+    double volatility;
+    double riskFreeRate;
+    double date;
+};
+
+class PutOption {
+public:
+    double strike;
+    double maturity;
+    double payoff(double stockAtMaturity) const;
+    double price (const BlackScholesModel& bsm) const;
+};
