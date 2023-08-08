a3699a127938a14aac0342245afe4940f3b81f16
minsun-ss <minsun@stuffofminsun.com>
2023-08-07 20:44:32 -0400

    finished the numerical integration i guess

--- Chapter10/NormalPDF.cpp
+++ Chapter10/NormalPDF.cpp
@@ -26,7 +26,18 @@ void testIntegralNormalPDF() {
     ASSERT_APPROX_EQUAL(actual, .95, .001);
 }
 
+/**
+ * Ex. 10.6.4
+ */
+void testNormCDF() {
+    NormalPDF npdf;
+    double pdfrange = integral(npdf, -1.96, 1.96, 1000);
+    double normcdfrange = normcdf(1.96) - normcdf(-1.96) ;
+    ASSERT_APPROX_EQUAL(pdfrange, normcdfrange, .01);
+}
+
 void testNormalPDF() {
     TEST(testPDF);
     TEST(testIntegralNormalPDF);
+    TEST(testNormCDF);
 }
