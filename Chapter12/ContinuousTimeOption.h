389c522854c002803c6964ba28bc5691b3e2f41b
minsun-ss <minsun@stuffofminsun.com>
2023-08-15 21:55:13 -0400

    Starting chapter 12

---
+++ Chapter12/ContinuousTimeOption.h
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+class ContinuousTimeOption {
+public:
+    virtual ~ContinuousTimeOption() {};
+}
---
+++ Chapter12/PathIndependentOption.h
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+class PathIndependentOption: public ContinuousTimeOptionBase {
+public:
+    virtual ~PathIndependentOption() {}
+    virtual double payoff(double endStockPrice) const = 0;
+    
+    double payoff (const std::vector<double>& stockPrices) const {
+        return payoff(stockPrices.back());
+    }
+    
+    bool isPathDependent() const {
+        return false;
+    };
+};
